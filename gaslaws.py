2461c63783b375c259c7f51a59d9594efadc7034
jteske54 <teske.jacob@gmail.com>
2015-10-07 17:54:45 -0500

    Initial commit

---
+++ gaslaws.py
@@ -0,0 +1,214 @@
+__author__ = 'jteske13'
+
+import tempconv, os
+
+def clear():
+    os.system('clear')
+
+def incorrect():
+    print("You have selected an incorrect number. Please select again.")
+
+def choice():
+    x = int(input("What is your unknown? "))
+    return x
+
+def xagain():
+    input("Press enter to do another conversion.")
+
+def menu():
+    clear()
+    print("""
+    Gas Law Conversions
+
+    1. Pressure and Volume
+    2. Volume and Temperature
+    3. Pressure and Temperature
+    4. Volume and n
+    5. Pressure, Volume, and Temperature
+    6. PV=nRT
+    7. Rate and Mass
+    8. Quit
+    """)
+    mc = (int(input("What would you like to do? Please enter a number: ")))
+    if mc == 1:
+        pressureVolume()
+    elif mc == 2:
+        volumeTemperature()
+    elif mc == 3:
+        pressureTemperature()
+    elif mc == 4:
+        volumeN()
+    elif mc == 5:
+        pressureVolumeTemperature()
+    elif mc == 6:
+        PVEqualsNRT()
+    elif mc == 7:
+        rateMass()
+    elif mc == 8:
+        print("Quitting...")
+        exit()
+    else:
+        print("That was not one of the options, please make another choice.")
+        wait = input("Please press enter to continue.")
+        menu()
+
+def pressureVolume():
+    clear()
+    print("""
+    Pressure and Volume
+
+    """)
+    print("""
+    Unknown:
+
+    1. Pressure
+    2. Volume
+    """)
+    z = choice()
+    if z == 1:
+        pressureVolumePressure()
+    elif z == 2:
+        pressureVolumeVolume()
+    else:
+        incorrect()
+        pressureVolume()
+
+def pressureVolumePressure():
+    clear()
+    print("Unknown Pressure")
+    P1 = float(input("P1= "))
+    V1 = float(input("V1= "))
+    V2 = float(input("V2= "))
+    P2 = (P1 * V1) / V2
+    print()
+    print("P2= " + str(P2))
+    xagain()
+    menu()
+
+def pressureVolumeVolume():
+    clear()
+    print("Unknown Volume")
+    P1 = float(input("P1= "))
+    V1 = float(input("V1= "))
+    P2 = float(input("V2= "))
+    V2 = (P1 * V1) / P2
+    print()
+    print("V2= " + str(V2))
+    input("Press enter to do another conversion.")
+    menu()
+
+def volumeTemperature():
+    clear()
+    print("Volume and Temperature")
+    print()
+    print("""
+    Unknown:
+
+    1. Temperature
+    2. Volume
+    """)
+    z = choice()
+    if z == 1:
+        volumeTemperatureTemperature()
+    elif z == 2:
+        volumeTemperatureVolume()
+    else:
+        incorrect()
+        volumeTemperature()
+
+def volumeTemperatureTemperature():
+    clear()
+    print("Unknown Temperature")
+    V1 = float(input("V1= "))
+    T1t = float(input("T1= "))
+    print("""
+    1. Kelvin
+    2. Celsius
+    3. Fahrenheit
+    """)
+    tempChoice = int(input("What are the units for T1? "))
+    if tempChoice == 1:
+        T1 = tempconv.xkelvin(T1t)
+    elif tempChoice == 2:
+        T1 = tempconv.xcelcius(T1t)
+    elif tempChoice == 3:
+        T1 = tempconv.xfahrenheit(T1t)
+    else:
+        incorrect()
+        input()
+        volumeTemperatureTemperature()
+    V2 = float(input("V2= "))
+    T2 = V2 / (V1 / T1)
+    tempconv.xkelvin(T2)
+    xagain()
+    menu()
+
+def volumeTemperatureVolume():
+    clear()
+    print("Unknown Volume")
+    V1 = float(input("V1= "))
+    T1t = float(input("T1= "))
+    print("""
+        1. Kelvin
+        2. Celsius
+        3. Fahrenheit
+        """)
+    tempChoice = int(input("What are the units for T1? "))
+    if tempChoice == 1:
+        T1 = tempconv.xkelvin(T1t)
+    elif tempChoice == 2:
+        T1 = tempconv.xcelcius(T1t)
+    elif tempChoice == 3:
+        T1 = tempconv.xfahrenheit(T1t)
+    else:
+        incorrect()
+        input()
+        volumeTemperatureVolume()
+    T2t = float(input("T2= "))
+    print("""
+        1. Kelvin
+        2. Celsius
+        3. Fahrenheit
+        """)
+    tempChoice = int(input("What are the units for T2? "))
+    if tempChoice == 1:
+        T2 = tempconv.xkelvin(T2t)
+    elif tempChoice == 2:
+        T2 = tempconv.xcelcius(T2t)
+    elif tempChoice == 3:
+        T2 = tempconv.xfahrenheit(T2t)
+    else:
+        incorrect()
+        input()
+        volumeTemperatureVolume()
+    V2 = (V1 / T1) * T2
+    print("V2= " + str(V2))
+    xagain()
+    menu()
+
+#def pressureTemperature():
+
+
+#def volumeN():
+
+
+#def pressureVolumeTemperature():
+
+
+#def PVEqualsNRT():
+
+
+#def rateMass():
+
+
+def main():
+    menu()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
---
+++ tempconv.py
@@ -0,0 +1,82 @@
+__author__ = 'jteske13'
+
+def incorrect():
+    print("You have selected an incorrect number. Please select again.")
+
+def tempConvChoose():
+    print("""
+    1. Kelvin
+    2. Celsius
+    3. Fahrenheit
+    """)
+    tempChoice = int(input("What would you like to convert? "))
+    if tempChoice == 1:
+        kelvin()
+    elif tempChoice == 2:
+        celcius()
+    elif tempChoice == 3:
+        fahrenheit()
+    else:
+        incorrect()
+        input()
+        tempConvChoose()
+
+def kelvin():
+    k = int(input("K= "))
+    c = k - 273
+    f = (c * 1.8) + 32
+    print("K= " + str(k))
+    print("ºC= " + str(c))
+    print("ºF= " + str(f))
+    return k
+
+def celcius():
+    c = int(input("ºC= "))
+    k = c + 273
+    f = (c * 1.8) + 32
+    print("K= " + str(k))
+    print("ºC= " + str(c))
+    print("ºF= " + str(f))
+    return k
+
+def fahrenheit():
+    f = int(input("ºF= "))
+    c = (f - 32) / 1.8
+    k = c + 273
+    print("K= " + str(k))
+    print("ºC= " + str(c))
+    print("ºF= " + str(f))
+    return k
+
+def xkelvin(pull):
+    k = float(pull)
+    c = k - 273
+    f = (c * 1.8) + 32
+    print("K= " + str(pull))
+    print("ºC= " + str(c))
+    print("ºF= " + str(f))
+    return k
+
+def xcelcius(pull):
+    c = float(pull)
+    k = c + 273
+    f = (c * 1.8) + 32
+    print("K= " + str(k))
+    print("ºC= " + str(c))
+    print("ºF= " + str(f))
+    return k
+
+def xfahrenheit(pull):
+    f = float(pull)
+    c = (f - 32) / 1.8
+    k = c + 273
+    print("K= " + str(k))
+    print("ºC= " + str(c))
+    print("ºF= " + str(f))
+    return k
+
+def main():
+    tempConvChoose()
+
+if __name__ == '__main__':
+    main()
